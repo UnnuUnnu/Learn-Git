93f04408eef433559f7bbad4f09c7472d2b474db
Minh Anh <phamminhanhbk@gmail.com>
2023-09-25 22:34:53 +0700

    ly thuyet ko can coi

---
+++ ly_thuyet/Modify_string.py
@@ -0,0 +1,13 @@
+# Upper case, Lower case
+a = "I'don't want to go to School"
+print(a.upper())
+print(a.lower())
+
+# Remove Whitespace --> strip()
+a = "  I don't want to go to school "
+print(a)
+# print(a.strip())
+
+# replace
+a = "Hello world"
+print(a.replace("world", "Minh Anh "))
---
+++ ly_thuyet/access_elements_of_string.py
@@ -0,0 +1,28 @@
+a = "Ra ngoai co gio thoi la dia cau dang xoa dau ban"
+# print(a[0])
+
+# b = a[0]
+# print(b)
+
+# for x in "banana":
+#     print(x)
+
+# txt = "The best things in life are free!"
+# print("free" in txt)
+
+# check if a phrase or character is presenting a string, we can use the keyword in, not presenting we can use keyword "not in"
+# a = "Ra ngoai co gio thoi la dia cau dang xoa dau ban"
+# b = "ban" in a
+# print(b)
+
+# Get the character from the startto position 5 (not included), tức lấy từ vị trí thứ 2 đến vị trí thứ 4
+b = "Hello, World!"
+print(b[2:5])
+
+#
+b = "Hello, World!"
+print(b[:5])
+
+#
+b = "Hello, World!"
+print(b[2:])
